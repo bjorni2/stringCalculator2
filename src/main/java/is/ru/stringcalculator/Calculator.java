c51b8ab64352fc40cac52b6afb576061fbd76ca7
Björn Ingi Baldvinsson <bjornb13@ru.is>
2014-10-16 14:42:32 +0000

    Moved summing to private function.

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -2,22 +2,22 @@ package is.ru.stringcalculator;
 
 public class Calculator {
 	public static int add(String text){
-		if(text.equals("")){
-			return 0;
-		}
-		else if(text.contains(",")){
-			String[] numbers = text.split(",");
-			int sum = 0;
-			for(String s : numbers){
-				sum += toInt(s);
-			}
-			return sum;
-		}
-		return toInt(text);
+		String[] numbers = text.split(",");
+		return sum(numbers);
 	}
 	
 	private static int toInt(String text){
+		if(text.isEmpty())
+			return 0;
 		return Integer.parseInt(text);
+	}
+	
+	private static int sum(String[] numbers){
+		int sum = 0;
+		for(String s : numbers){
+			sum += toInt(s);
+		}
+		return sum;
 	}
 	
 }
